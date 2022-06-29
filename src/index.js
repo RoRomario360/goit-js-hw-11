716b227f92ce20331f58fde56a6747ad4b91e8e4
RoRomario360 <fotoromario@gmail.com>
2022-06-29 22:52:08 +0300

    commit-axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
+import fetchPhotos from './js/api/fetchPhoto';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import './css/styles.css';
+Notiflix.Notify.init({
+  width: '300px',
+  position: 'right-top',
+  closeButton: false,
+});
+
+//refs
+
+const searchForm = document.querySelector('#search-form');
+const galleryBox = document.querySelector('.gallery');
+//addlistners
+searchForm.addEventListener('submit', onFormSubmit);
+
+//form
+function onFormSubmit(e) {
+  e.preventDefault();
+  const query = e.target.elements.searchQuery.value;
+
+  fetchPhotos(query).then(response => {
+    if (response.data.hits.length === 0) {
+      Notiflix.Notify.failure('Oops');
+    }
+    renderCards(response.data.hits);
+  });
+}
+//render
+
+function renderCards(images) {
+  const listOfPhotos = images
+    .map(image => {
+      return `<div class="photo-card">
+  <a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${image.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${image.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${image.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${image.downloads}</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+  galleryBox.insertAdjacentHTML('afterbegin', listOfPhotos);
+  const lightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+  });
+}
---
+++ src/js/api/fetchPhoto.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+export default function fetchPhotos(value, page) {
+  return axios({
+    url: ' https:pixabay.com/api/',
+    params: {
+      key: '28345018-0c1af10fb3ec556a31002db0e',
+      q: value,
+      orientation: 'horizontal',
+      image_type: 'photo',
+      safesearch: true,
+      page: 1,
+      per_page: 40,
+    },
+  });
+}
+//   const BASE_URL = 'https://pixabay.com/api/';
+//   const key = '28345018-0c1af10fb3ec556a31002db0e';
+//   //FIXME:
+//   const searchUrl =
+//     '&image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
+
+//   const search = fetch(
+//     `${BASE_URL}?key=${key}&q=${value}${searchUrl}&page=${page}`
+//   )
+//     .then(data => {
+//       if (!data.ok) {
+//         throw new Error(data.status);
+//       }
+//       return data.json();
+//     })
+//     .catch(error => {
+//       Notiflix.Notify.failure('Oops, there is no country with that name');
+//     });
+//   return search;
